2f839b37ba58e9d18fbf0e6c8a3acc3523b8dbbf
Seto <setoapriawan69@gmail.com>
2023-09-24 14:37:48 +0700

    Main menu update and Adding phone specification menu

---
+++ app/src/main/java/com/excal/projectc/ui/PhoneDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.excal.projectc.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.excal.projectc.R
+
+class PhoneDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phone_detail)
+    }
+}
